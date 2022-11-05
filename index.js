6ec0c4396dfcb2d49ec445328556cca4910fb775
Le Van Huy <le.van.huy@azoom.jp>
2022-11-05 11:35:26 +0900

    fix

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const app = express();
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const port = process.env.PORT || 3000;
-
 app.use(
   cors({
     origin: true,
-  }),
-  express.urlencoded({ extended: true }),
-  express.json(),
-  express.text()
+    credentials: true,
+  })
 );
+app.use(express.urlencoded({ extended: true }), express.json(), express.text());
 app.use("/api", mainRouter);
 app.use((error, req, res, next) => {
   console.error(error);
--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,14 @@ router.get("/", async (req, res) => {
 router.get("/about", (req, res) => {
   res.send("About birds");
 });
+router.get("/interests", async (req, res) => {
+  const [rows] = await connection.query(`SELECT * FROM interests`);
+  return res.send(rows);
+});
+
+router.get("/job", async (req, res) => {
+  const [rows] = await connection.query(`SELECT * FROM job`);
+  return res.send(rows);
+});
 
 module.exports = router;
