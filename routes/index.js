a5ba56857a8064828c7b57f760277aae8b9e8845
Le Van Huy <le.van.huy@azoom.jp>
2022-11-06 20:11:30 +0900

    update logic code

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const { getConnection } = require("../database");
 const { kbs } = require("../helpers/kbs");
 const { getRules } = require("../helpers/rule");
 const connection = getConnection();
+
 router.get("/kbs", async (req, res) => {
   const rules = getRules();
   const { gt } = req.query;
   const input = gt.split(",");
   const ids = kbs(rules, input);
-  console.log(ids);
   const query = ids.map((id) => `'${id}'`).join(",");
   const [rows] = await connection.query(
     `SELECT * FROM mobile where id IN (${query})`
@@ -18,9 +18,6 @@ router.get("/kbs", async (req, res) => {
   res.send(rows);
 });
 
-router.get("/about", (req, res) => {
-  res.send("About birds");
-});
 router.get("/interests", async (req, res) => {
   const [rows] = await connection.query(`SELECT * FROM interests`);
   return res.send(rows);
@@ -28,6 +25,11 @@ router.get("/interests", async (req, res) => {
 
 router.get("/job", async (req, res) => {
   const [rows] = await connection.query(`SELECT * FROM job`);
+  return res.send(rows);
+});
+
+router.get("/mobile", async (req, res) => {
+  const [rows] = await connection.query(`SELECT * FROM mobile`);
   return res.send(rows);
 });
 
